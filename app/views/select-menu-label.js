a2a995b8d51bf452b28ae61eb80c016480d43d4f
Tim Evans <tim-evans@sproutcore.com>
2014-10-29 22:21:25 -0400

    make views public to the consuming application

---
+++ app/views/select-menu-label.js
@@ -0,0 +1,2 @@
+import SelectMenuLabel from "ember-select-menu/views/select-menu-label";
+export default SelectMenuLabel;
---
+++ app/views/select-menu-list.js
@@ -0,0 +1,2 @@
+import SelectMenuList from "ember-select-menu/views/select-menu-list";
+export default SelectMenuList;
