30dea036731ece38b437247034f5e34a862e17c3
Tim Evans <tim.evans@paddle8.com>
2014-10-31 14:07:46 -0400

    don't instrument selection; just set the html
    TODO: find a way to detect mutations to the active descendant and
    update the label appropriately. Instrumentation seems like the
    incorrect approach here, so I removed it

--- addon/views/select-menu-label.js
+++ addon/views/select-menu-label.js
@@ -7,10 +7,7 @@ var set = Ember.set;
 var reads = Ember.computed.reads;
 var not = Ember.computed.not;
 
-var subscribe = Ember.subscribe;
-var unsubscribe = Ember.unsubscribe;
 var scheduleOnce = Ember.run.scheduleOnce;
-var cancel = Ember.run.cancel;
 
 var SelectMenuLabel = Ember.View.extend({
 
@@ -29,21 +26,16 @@ var SelectMenuLabel = Ember.View.extend({
                       'menu.isActive:active',
                       'isHovering:hover'],
 
+  menu: nearestParent("select-menu"),
+  prompt: reads('menu.prompt'),
+
   ariaRole: 'button',
   "aria-haspopup": 'true',
   "aria-owns": reads('menu.list.elementId'),
   "aria-disabled": stringify("menu.disabled"),
   "aria-expanded": stringify("menu.isActive"),
 
-  menu: nearestParent("select-menu"),
-
-  registerView: function () {
-    set(this, 'menu.label', this);
-  }.on('didInsertElement'),
-
   tabindex: 0,
-
-  prompt: reads('menu.prompt'),
 
   mouseEnter: function () {
     set(this, 'isHovering', true);
@@ -60,39 +52,13 @@ var SelectMenuLabel = Ember.View.extend({
   activeDescendant: reads('menu.activeDescendant'),
   isPrompting: not('activeDescendant'),
 
-  instrumentActiveDescendant: function () {
-    var view = this;
-    this.instrumentor = subscribe("render.component.active-descendant", {
-      after: function () {
-        var activeDescendant = get(view, 'activeDescendant');
-        set(view, 'value', activeDescendant.$().html());
-      }
-    });
-  }.on('init'),
-
-  removeInstrumentation: function () {
-    unsubscribe(this.instrumentor);
-  }.on('destroy'),
-
-  // Schedule a rerender of the label when the active desencdant changes
   activeDescendantDidChange: function () {
-    this._timer = scheduleOnce('afterRender', this, 'updateLabel');
+    scheduleOnce('afterRender', this, 'sync');
   }.observes('activeDescendant').on('init'),
 
-  activeDescendantWillChange: function () {
-    cancel(this._timer);
+  sync: function () {
     var activeDescendant = get(this, 'activeDescendant');
-    if (activeDescendant) {
-      activeDescendant.instrumentName = 'component';
-    }
-  }.observesBefore('activeDescendant'),
-
-  updateLabel: function () {
-    var activeDescendant = get(this, 'activeDescendant');
-    if (activeDescendant) {
-      activeDescendant.instrumentName = 'component.active-descendant';
-      set(this, 'value', activeDescendant.$().html());
-    }
+    set(this, 'value', activeDescendant ? activeDescendant.$().html() : null);
   }
 
 });
