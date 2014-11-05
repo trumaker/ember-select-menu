733d504e165d4bfaae16573ede8f3560db6c372d
Tim Evans <tim.evans@paddle8.com>
2014-11-05 11:38:13 -0500

    add tests for scrolling using arrow keys

--- tests/acceptance/scroll-test.js
+++ tests/acceptance/scroll-test.js
@@ -14,13 +14,52 @@ module("Acceptance: options scroll into view", {
 });
 
 test("searching for an option will focus it into view", function () {
-  expect(1);
+  expect(2);
   visit("/");
   keyEvent(".select-menu:last-child", "keydown", "z");
   andThen(function () {
     var popup = find("#alphabet + .popup-menu");
     var list = popup.find(".select-menu_list");
     var maxScrollTop = getLayout(list[0]).padding.height - getLayout(popup[0]).height;
-    equal(popup.scrollTop(), maxScrollTop);
+    equal(list.scrollTop(), maxScrollTop);
   });
+
+  keyEvent(".select-menu:last-child", "keydown", "a");
+  andThen(function () {
+    var popup = find("#alphabet + .popup-menu");
+    equal(popup.scrollTop(), 0);
+  });
+});
+
+test("using up and down arrows will focus the element into view", function () {
+  expect(15);
+  visit("/");
+
+  var UP = 38;
+  var DOWN = 40;
+
+  var flushTop = function () {
+    var list = find("#alphabet + .popup-menu .select-menu_list");
+    equal(list.scrollTop(), 0);
+  };
+  var offset = function () {
+    var list = find("#alphabet + .popup-menu .select-menu_list");
+    ok(list.scrollTop() > 0);
+  };
+
+  for (var i = 0; i < 7; i++) {
+    keyEvent(".select-menu:last-child", "keydown", DOWN);
+    andThen(flushTop);
+  }
+
+  keyEvent(".select-menu:last-child", "keydown", DOWN);
+  andThen(offset);
+
+  for (i = 0; i < 6; i++) {
+    keyEvent(".select-menu:last-child", "keydown", UP);
+    andThen(offset);
+  }
+
+  keyEvent(".select-menu:last-child", "keydown", UP);
+  andThen(flushTop);
 });
