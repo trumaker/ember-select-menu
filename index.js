0a3fa37f50c99e563e427acad3eea4301e6271f5
Tim Evans <tim.evans@paddle8.com>
2014-11-14 13:12:39 -0500

    fix #1 by importing css using vendor

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-select-menu'
+  name: 'ember-select-menu',
+
+  included: function (app) {
+    this._super.included(app);
+    app.import("vendor/styles/ember-select-menu.css");
+  }
 };
